19d94ca2de6830f4fcb4087950ea66a4700edbe3
Hexagonist <januszkrem2@wp.pl>
2024-11-17 20:09:02 +0100

    Lab 2.1 works

--- Lista_2/app/src/main/java/com/example/lista2/FragmentA.kt
+++ Lista_2/app/src/main/java/com/example/lista2/FragmentA.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+
 import com.example.lista2.databinding.FragmentABinding
 
 class FragmentA : Fragment() {
--- Lista_2/app/src/main/java/com/example/lista2/FragmentB.kt
+++ Lista_2/app/src/main/java/com/example/lista2/FragmentB.kt
@@ -5,20 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.lista2.databinding.FragmentABinding
 import com.example.lista2.databinding.FragmentBBinding
 
 class FragmentB : Fragment() {
 
     private lateinit var binding: FragmentBBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
         binding = FragmentBBinding.inflate(inflater)
-        binding.textview.text=arguments?.getInt("value").toString()
+
+        binding.textview.text = arguments?.getInt("value").toString() // odebranie danych
+
+        binding.fabB.setOnClickListener {
+            val action = FragmentBDirections.actionFragmentBToFragmentA2()
+            Navigation.findNavController(requireView()).navigate(action)
+        }
+
         return binding.root
     }
-
 }
