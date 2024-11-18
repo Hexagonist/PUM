409cd8946e66d8eb65be4d9083a414c230fc8666
Hexagonist <januszkrem2@wp.pl>
2024-11-18 02:19:51 +0100

    Registration works

--- Lista_2/app/src/main/java/com/example/lista2/FragmentRegister.kt
+++ Lista_2/app/src/main/java/com/example/lista2/FragmentRegister.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
 import com.example.lista2.databinding.FragmentRegisterBinding
+import androidx.fragment.app.activityViewModels
 
 class FragmentRegister : Fragment() {
 
     private lateinit var binding: FragmentRegisterBinding
+
+    // Shared ViewModel instance
+    private val usersModel: UsersViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,8 +31,28 @@ class FragmentRegister : Fragment() {
 //        }
 
         binding.buttonRegister.setOnClickListener{
-            val action = FragmentRegisterDirections.actionFragmentRegisterToFragmentLogin()
-            Navigation.findNavController(requireView()).navigate(action)
+            var input_login = binding.textInputLogin.editText?.text.toString()
+            var input_password = binding.textInputHaslo.editText?.text.toString()
+            var input_repeatedPassword = binding.textInputPowtorzHaslo.editText?.text.toString()
+            var all_users = usersModel.getUsers()
+
+            // User already exists
+            if( all_users.any{it.login == input_login} )
+            {
+                Toast.makeText(requireContext(), "User $input_login already exists!", Toast.LENGTH_SHORT).show()
+            }
+            // wrong repeated password
+            else if(input_password != input_repeatedPassword)
+            {
+                Toast.makeText(requireContext(), "Passwords must be the same!", Toast.LENGTH_SHORT).show()
+            }
+            // success
+            else
+            {
+                usersModel.addUser(User(input_login, input_password))
+                val action = FragmentRegisterDirections.actionFragmentRegisterToFragmentLogin()
+                Navigation.findNavController(requireView()).navigate(action)
+            }
         }
 
         return binding.root
